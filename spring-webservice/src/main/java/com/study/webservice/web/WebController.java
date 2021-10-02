fd1b36eebc2b13747f2690ca9fe495659326360e
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2021-10-03 05:53:47 +0900

    1. Spring boot 시작 환경 구축 2. User 정보를 중심으로한 Spring MVC 구현 3. Spring Security & Google OAuth를 통한 로그인 기능 구현 4. 간단한 홈페이지 클라이언트 구현

---
+++ spring-webservice/src/main/java/com/study/webservice/web/WebController.java
@@ -0,0 +1,34 @@
+package com.study.webservice.web;
+
+import com.study.webservice.config.auth.LoginUser;
+import com.study.webservice.config.auth.dto.SessionUser;
+import lombok.AllArgsConstructor;;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    @GetMapping("/")
+    public String main(Model model, @LoginUser SessionUser user) {
+
+        // 사용자 정보: 위의 @LoginUser 어노테이션으로 대체
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("guestName", user.getName());
+            model.addAttribute("guestImg", user.getPicture());
+        }
+        return "index";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage() { return "mypage"; }
+
+}
---
+++ spring-webservice/src/main/java/com/study/webservice/web/WebRestController.java
@@ -0,0 +1,9 @@
+package com.study.webservice.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+}
